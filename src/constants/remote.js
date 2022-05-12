ee2bc2e5eaf3d4eb500b7704d42d7852f05d09fb
Tiago Moraes <tiago3902@gmail.com>
2022-05-11 22:58:58 -0300

    modificado o api url

--- src/constants/remote.js
+++ src/constants/remote.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://stemgame-api.herokuapp.com';
 export const STORAGE_URL = 'https://stemgame.s3.us-east-2.amazonaws.com/assets';
